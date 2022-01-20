8e7f623d003bb756e81f597927824ba85764fbd9
nico0891 <nico150891@gmail.com>
2022-01-20 16:02:41 -0300

    Panel admin

--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -5,8 +5,11 @@ from django.db import models
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(verbose_name='Correo') #Cambia texto panel Django
     tfno = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
